4345ea591a20fd09afd4bfa8b864c013becba931
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2022-02-08 15:03:15 +0100

    Improve the thank you page.

--- front-end/js/experiment-thanks.js
+++ front-end/js/experiment-thanks.js
@@ -9,7 +9,10 @@
     var queryParas = periscope.util.parseVars(window.location.search);
     var userPlatformId = queryParas["PROLIFIC_PID"];
     if (typeof userPlatformId !== "undefined") {
+      $("#prolific-redirect-text").show();
       $("#prolific-redirect").show();
+    } else {
+      $("#thank-text").show();
     }
   }
   $(init);
