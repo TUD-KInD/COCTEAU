320a69804fcea2e886de547ef7848747c516bdfb
root at periscope.io.tudelft.nl <root@linux-template-ubuntu-1804-camp.tudelft.nl>
2022-06-15 11:51:54 +0200

    Improve the script for printing prolific user data

--- back-end/www/print_prolific_data.py
+++ back-end/www/print_prolific_data.py
@@ -8,17 +8,23 @@ if len(sys.argv)>1:
 def print_one(d, i):
     print(d.iloc[i].to_frame())
     print("")
-    print("Data quality check 1:")
+    print("Data quality check 1 (creating a vision meme):")
     print(d.iloc[i]["motivation_url_0"])
     print(d.iloc[i]["motivation_description_0"])
     print("")
-    print("Data quality check 2 (reversed question):")
+    print("Data quality check 2 (reversed question)")
     print(d.iloc[i]["Before criticizing somebody, I try to imagine how I would feel if I were in their place."])
     print(d.iloc[i]["I do not try to imagine how I would feel if I were in somebody's place before criticizing them."])
     print("")
     print("Data quality check 3 (reversed question):")
     print(d.iloc[i]["Sometimes I don't feel sorry for other people when they are having problems."])
     print(d.iloc[i]["Sometimes I feel sorry for other people when they are having problems."])
+    print("")
+    print("Attention check 1 (should have value 1.0):")
+    print(d.iloc[i]["Please select the option Strongly Agree from the list of choices below."])
+    print("")
+    print("Attention check 2 (should have value \"I confirm my opinion\"):")
+    print(d.iloc[i]["Please enter \"I confirm my opinion\" to the text box below."])
 
 d = pd.read_csv("prolific-data.csv")
 for i in range(len(d)):
@@ -30,3 +36,5 @@ for i in range(len(d)):
     else:
         if d.iloc[i]["prolific_id"] == pid:
             print_one(d, i)
+print("")
+print("END")
